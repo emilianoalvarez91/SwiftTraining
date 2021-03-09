19a4efba7cf350cf105c62f4a33f16d22a8766d3
abdullah <abdullah.saeed@coolblue.nl>
2021-03-09 15:58:48 +0100

    Succeed and Fail

--- SwiftUITraining/SwiftUITraining/Data/PlanetService.swift
+++ SwiftUITraining/SwiftUITraining/Data/PlanetService.swift
@@ -8,18 +8,33 @@
 import Foundation
 import Alamofire
 
+protocol PlanetServiceProtocol {
+  func planets(completion: @escaping ([Planet]) -> Void)
+}
 
-struct PlanetService {
+struct PlanetService: PlanetServiceProtocol {
   let url = "https://y3fsc8hysh.execute-api.us-east-2.amazonaws.com/training/planets"
-    
-  func planets(completion: @escaping ([Planet]) -> Void ) {
-        AF.request(url).responseDecodable(of: [Planet].self) { (response) in
-            switch(response.result) {
-            case .success(let planets):
-              completion(planets)
-            case .failure(let error):
-                print(error)
-            }
-        }
+
+  func planets(completion: @escaping ([Planet]) -> Void) {
+    AF.request(url).responseDecodable(of: [Planet].self) { (response) in
+      switch(response.result) {
+      case .success(let planets):
+        completion(planets)
+      case .failure(let error):
+        completion([])
+      }
     }
+  }
+}
+
+struct StubPlanetServiceSucceed: PlanetServiceProtocol {
+  func planets(completion: @escaping ([Planet]) -> Void) {
+    completion(PlanetMockData.planets)
+  }
+}
+
+struct StubPlanetServiceFail: PlanetServiceProtocol {
+  func planets(completion: @escaping ([Planet]) -> Void) {
+    completion([])
+  }
 }
