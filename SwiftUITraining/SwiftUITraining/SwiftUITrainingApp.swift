72364d5b324b683b2d59d9d8f31cfe27273bd31a
Emiliano Alvarez <emiliano.alvarez@coolblue.nl>
2021-03-09 09:22:06 -0300

    Initial project setup

---
+++ SwiftUITraining/SwiftUITraining/SwiftUITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainingApp.swift
+//  SwiftUITraining
+//
+//  Created by Emiliano Alvarez on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
